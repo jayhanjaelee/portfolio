8ff87d5f110d10202190ae3484a3369c67e85c26
Hanjae Lee <jayhanjaelee@gmail.com>
2023-07-20 19:50:36 +0900

    Add: layout.
    - [x] Project section
    - [x] Contact section
    - [x] Mobile Menu
    - [x] dark and light mode
    - [x] image lazy lazyloading
    - [x] custom scrollbar

---
+++ src/utils/dark-mode.js
@@ -0,0 +1,25 @@
+const darkMode = () => {
+  const themeToggleBtn = document.querySelectorAll("#theme-toggle");
+
+  // State
+  const theme = localStorage.getItem("theme");
+
+  // On mount
+  theme && document.body.classList.add(theme);
+
+  // Handlers
+  const handleThemeToggle = () => {
+    document.body.classList.toggle("light-mode");
+    if (document.body.classList.contains("light-mode")) {
+      localStorage.setItem("theme", "light-mode");
+    } else {
+      localStorage.removeItem("theme");
+      document.body.removeAttribute("class");
+    }
+  };
+
+  // Events
+  themeToggleBtn.forEach((btn) => btn.addEventListener("click", handleThemeToggle));
+};
+
+export default darkMode;
